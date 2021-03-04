eb41225a8770479be25b90c5b53445c31436e654
Fabio Mignogna <mig.fabio@gmail.com>
2021-03-04 23:09:20 +0000

    Created Couchbase Lite internal DTO to make the code cleaner

--- Tests/FeedStoreChallengeTests.swift
+++ Tests/FeedStoreChallengeTests.swift
@@ -7,6 +7,47 @@ import FeedStoreChallenge
 import CouchbaseLiteSwift
 
 class CouchbaseLiteFeedStore: FeedStore {
+	private struct Cache {
+		private let feed: [CouchbaseLiteFeedImage]
+		private let timestamp: Date
+
+		init(feed: [LocalFeedImage], timestamp: Date) {
+			self.feed = feed.map(CouchbaseLiteFeedImage.init)
+			self.timestamp = timestamp
+		}
+
+		var toDocument: MutableDocument {
+			MutableDocument()
+				.setArray(MutableArrayObject(data: feed.map { $0.toDictionaryObject }), forKey: "feed")
+				.setDouble(timestamp.timeIntervalSinceReferenceDate, forKey: "timestamp")
+		}
+	}
+
+	private struct CouchbaseLiteFeedImage {
+		private let id: UUID
+		private let description: String?
+		private let location: String?
+		private let url: URL
+
+		var toDictionaryObject: DictionaryObject {
+			MutableDictionaryObject(
+				data: [
+					"id": id.uuidString,
+					"description": description,
+					"location": location,
+					"url": url.absoluteString
+				].compactMapValues { $0 }
+			)
+		}
+
+		init(localFeedImage: LocalFeedImage) {
+			self.id = localFeedImage.id
+			self.description = localFeedImage.description
+			self.location = localFeedImage.location
+			self.url = localFeedImage.url
+		}
+	}
+
 	private lazy var database: Database = {
 		let dbConfig = DatabaseConfiguration()
 		dbConfig.directory = storeURL.path
@@ -22,23 +63,8 @@ class CouchbaseLiteFeedStore: FeedStore {
 	func deleteCachedFeed(completion: @escaping DeletionCompletion) {}
 
 	func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-		let dbItems: [MutableDictionaryObject] = feed.map { item -> [String: Any] in
-			let json: [String: Any?] = [
-				"id": item.id.uuidString,
-				"description": item.description,
-				"location": item.location,
-				"url": item.url.absoluteString
-			]
-			return json.compactMapValues { $0 }
-		}
-		.map { MutableDictionaryObject(data: $0) }
-
-		let cache = MutableDocument()
-			.setArray(MutableArrayObject(data: dbItems), forKey: "feed")
-			.setDouble(timestamp.timeIntervalSinceReferenceDate, forKey: "timestamp")
-
-		try! database.saveDocument(cache)
-
+		let cache = Cache(feed: feed, timestamp: timestamp)
+		try! database.saveDocument(cache.toDocument)
 		completion(nil)
 	}
 
@@ -47,27 +73,32 @@ class CouchbaseLiteFeedStore: FeedStore {
 			.select(SelectResult.all())
 			.from(DataSource.database(database))
 
-		if let results = try? query.execute(),
-		   let cache = results.allResults().first?.dictionary(forKey: "feed-store"),
-		   let feed = cache.array(forKey: "feed")?.toArray() as? [[String: Any]] {
-			let localFeed = feed.compactMap { item -> LocalFeedImage? in
-				guard let idString = item["id"] as? String, let id = UUID(uuidString: idString),
-					let description = item["description"] as? String?,
-					let location = item["location"] as? String?,
-					let urlString = item["url"] as? String, let url = URL(string: urlString) else {
-					return nil
-				}
-				return LocalFeedImage(id: id, description: description, location: location, url: url)
-			}
-			completion(
-				.found(
-					feed: localFeed,
-					timestamp: Date(timeIntervalSinceReferenceDate: cache.double(forKey: "timestamp"))
-				)
-			)
-		} else {
-			completion(.empty)
+		guard let results = try? query.execute(),
+			  let result = results.allResults().first?.dictionary(forKey: "feed-store"),
+			  let feedArray = result.array(forKey: "feed")?.toArray() as? [[String: Any]]
+		else {
+			return completion(.empty)
 		}
+
+		let feed = makeLocalFeedImages(from: feedArray)
+		let timestamp = makeTimestamp(from:  result.double(forKey: "timestamp"))
+		completion(.found(feed: feed, timestamp: timestamp))
+	}
+
+	private func makeLocalFeedImages(from result: [[String: Any]]) -> [LocalFeedImage] {
+		result.compactMap {
+			guard let idString = $0["id"] as? String, let id = UUID(uuidString: idString),
+				  let description = $0["description"] as? String?,
+				  let location = $0["location"] as? String?,
+				  let urlString = $0["url"] as? String, let url = URL(string: urlString) else {
+				return nil
+			}
+			return LocalFeedImage(id: id, description: description, location: location, url: url)
+		}
+	}
+
+	private func makeTimestamp(from result: TimeInterval) -> Date {
+		Date(timeIntervalSinceReferenceDate: result)
 	}
 }
 
